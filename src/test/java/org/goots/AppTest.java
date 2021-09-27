e1cc8e4cd78b79a33bd5743025871c732eb318de
Nick Cross <ncross@redhat.com>
2021-09-27 12:21:29 +0100

    Initial code commit

---
+++ src/test/java/org/goots/AppTest.java
@@ -0,0 +1,20 @@
+package org.goots;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
